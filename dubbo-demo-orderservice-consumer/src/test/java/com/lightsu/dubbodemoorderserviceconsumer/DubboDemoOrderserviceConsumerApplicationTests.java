03e86508533cd5db2a788044735415f86d386f6c
lubaikang <332138298@qq.com>
2020-03-23 09:25:58 +0800

    update

---
+++ dubbo-demo-orderservice-consumer/src/test/java/com/lightsu/dubbodemoorderserviceconsumer/DubboDemoOrderserviceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightsu.dubbodemoorderserviceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboDemoOrderserviceConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
